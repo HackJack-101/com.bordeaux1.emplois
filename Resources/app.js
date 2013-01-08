f9c75c7ae501345fb6463b493ba5dedf06d221e4
Jack3113 <jack03113@gmail.com>
2013-01-08 20:39:07 +0100

    Fix bugs and add some useful menu items

--- Resources/app.js
+++ Resources/app.js
@@ -1,8 +1,14 @@
-Ti.UI.setUsingChrome(true)
-
 var menu = Ti.UI.createMenu();
 
 programItem = Ti.UI.createMenuItem('Programme');
+homeItem = programItem.addItem('Accueil', function() {
+  Ti.UI.getCurrentWindow().setURL('app://index.html');
+});
+restartItem = programItem.addItem('Redémarrer', function() {
+  if (confirm('Êtes vous sûr de vouloir redémarrer ?')) {
+    Ti.App.restart();
+  }
+});
 exitItem = programItem.addItem('Quitter', function() {
   if (confirm('Êtes vous sûr de vouloir quitter ?')) {
     Ti.App.exit();
@@ -12,16 +18,16 @@ menu.appendItem(programItem);
 
 optionsItem = Ti.UI.createMenuItem('Options');
 settingsItem = optionsItem.addItem('Configuration', function() {
-  Ti.UI.setURL('app://settings.html')
+  Ti.UI.getCurrentWindow().setURL('app://settings.html');
 });
 testItem = optionsItem.addItem('Test', function() {
-  Ti.UI.setURL('app://test.html')
+  Ti.UI.getCurrentWindow().setURL('app://test.html');
 });
 menu.appendItem(optionsItem);
 
 Ti.UI.setMenu(menu);
 
-/*
+
 //Create a callback function for the notification
 var doSomething = function(e) {
     //e.hide();
@@ -30,8 +36,10 @@ var doSomething = function(e) {
 //Creating a notification and displaying it.
 var notification = Ti.Notification.createNotification({
     'title' : 'Notification d\'ouverture',
-    'message' : 'Bienvenue sur sur l\'application desktop des emplois du temps de Bordeaux 1!',
+    'message' : 'Bienvenue sur sur l\'application desktop des emplois du temps de Bordeaux 1 !',
     'timeout' : 20,
     'callback' : doSomething,
     'icon' : 'app://ico/apple-touch-icon.png'        
-});*/
+});
+
+window.addEventListener('load',notification.show, true);
