00a63ac6e1e3d120f4641187f2d4b8fb6c442bd9
Jack3113 <jack03113@gmail.com>
2013-01-08 20:27:59 +0100

    Add Menu Items

--- Resources/app.js
+++ Resources/app.js
@@ -1,23 +1,27 @@
+Ti.UI.setUsingChrome(true)
+
 var menu = Ti.UI.createMenu();
-programmItem = Ti.UI.createMenuItem('Programme');
-exitItem = programmItem.addItem('Quitter', function() {
+
+programItem = Ti.UI.createMenuItem('Programme');
+exitItem = programItem.addItem('Quitter', function() {
   if (confirm('Êtes vous sûr de vouloir quitter ?')) {
     Ti.App.exit();
   }
 });
-menu.appendItem(fileItem);
+menu.appendItem(programItem);
 
-optionsItem = Ti.UI.createMenuItem('Programme');
-exitItem = programmItem.addItem('Quitter', function() {
-  if (confirm('Êtes vous sûr de vouloir quitter ?')) {
-    Ti.App.exit();
-  }
+optionsItem = Ti.UI.createMenuItem('Options');
+settingsItem = optionsItem.addItem('Configuration', function() {
+  Ti.UI.setURL('app://settings.html')
 });
+testItem = optionsItem.addItem('Test', function() {
+  Ti.UI.setURL('app://test.html')
+});
+menu.appendItem(optionsItem);
 
-menu.appendItem(fileItem);
 Ti.UI.setMenu(menu);
 
-
+/*
 //Create a callback function for the notification
 var doSomething = function(e) {
     //e.hide();
@@ -30,4 +34,4 @@ var notification = Ti.Notification.createNotification({
     'timeout' : 20,
     'callback' : doSomething,
     'icon' : 'app://ico/apple-touch-icon.png'        
-});
+});*/
